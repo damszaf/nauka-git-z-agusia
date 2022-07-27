ec7eb89124edb1bcc08fee40653f4e7160272f85
Damian Szafraniec <damszafr@publicisgroupe.net>
2022-07-27 15:02:23 +0200

    Add third number

--- nauka-gita.js
+++ nauka-gita.js
@@ -1,5 +1,5 @@
 let x = 10;
 
-function add(firstNumber, secondNumer) {
-    return firstNumber + secondNumer;
+function add(firstNumber, secondNumer, thirdNumber) {
+    return firstNumber + secondNumer + thirdNumber;
 }
